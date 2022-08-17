15e5ad79c371d230034f1eeda9d8b3c663911941
JawadShk <shaikhjawad020@gmail.com>
2022-08-17 20:11:03 +0530

    variables completed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,61 @@
 // alert("hello world");
+// prompt("hello world");
 // document.write("jawad");
 // console.log("jawad");
-console.log(5+6);
-alert(5+6);
+// console.log(5+6);
+// alert(5+6);
+// document.getElementById("head").style.color="red";
+
+// 1.statements in java script(every line is a statement like in local languages)
+/*  var a;
+    a = 5;
+    alert(a);
+*/
+
+// 2.Commments in java script (every thing inside the comment will be ignored by javascript and will not b executed)
+// single line Comment
+// x=5; 
+
+// multiple line comments 
+/*  var a;
+    a = 5;
+    alert(a);
+*/
+
+// 3.variables in javascript (variables are used for storing values. the values which will be reused further line in mathematics we give x=2 we are storing value 2 inside x and resuing it whereever we want eg.x+2)
+
+/*Rules for declaring a variable 
+
+    variable names must be inside single or double quotes
+    Variable names cannot contain spaces.
+    Variable names must begin with a letter, an underscore (_) or a dollar sign ($).
+    Variable names can only contain letters, numbers, underscores, or dollar signs.
+    Variable names are case-sensitive.
+    Certain words may not be used as variable names called reserved words, because they have other meanings within JavaScript.
+*/
+
+/*  var digit1 = 5;
+    alert(digit1); 
+*/
+// whenever we are working with variable we should not use double/single quotes in alert as double quotes/single quotes are used for string 
+
+/*  var digit1 = 6;
+    var x = 2;
+    var z = digit1 + x + 2;
+    console.log(z);
+*/
+// var is the old method used for declaring/initializing variable.After ES6 let and const were introduce
+/*  let digit1 =6;
+    alert(digit1);
+*/
+
+// const is used for making a rule  --> in var & let we can change the value assigned to the variale but we cannot do the same for const
+/*  price1=5;
+    price2=6
+    let = price1 + price2;
+*/
+
+/*  const digit2 = 6;
+    digit2 =8;
+    alert(digit2);
+*/
