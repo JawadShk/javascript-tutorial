1a42065613b760cd379397053a0e337fc7fc67d6
JawadShk <shaikhjawad020@gmail.com>
2022-08-18 13:32:11 +0530

    operators completed

--- js/script.js
+++ js/script.js
@@ -59,3 +59,150 @@
     digit2 =8;
     alert(digit2);
 */
+
+// 4.operators in javascript
+
+// The assignment operator (=) assigns a value to a variable.
+// let x = 10;
+
+// 1: The addition operator (+) adds numbers:
+/*    let x = 5;
+    let y = 3;
+
+    // addition
+    console.log('x + y = ', x + y);  // 8
+
+    // subtraction
+    console.log('x - y = ', x - y);  // 2
+
+    // multiplication
+    console.log('x * y = ', x * y);  // 15
+
+    // division
+    console.log('x / y = ', x / y);  // 1.6666666666666667
+
+    // remainder
+    console.log('x % y = ', x % y);   // 2
+
+    // increment
+    console.log('++x = ', ++x); // x is now 6
+    console.log('x++ = ', x++); // prints 6 and then increased to 7
+    console.log('x = ', x);     // 7
+
+    // decrement
+    console.log('--x = ', --x); // x is now 6
+    console.log('x-- = ', x--); // prints 6 and then decreased to 5
+    console.log('x = ', x);     // 5
+*/
+
+// 2: Comparison operators in JavaScript
+
+/*  //exponentiation
+    console.log('x ** y =', x ** y);
+
+    // equal operator
+    console.log(2 == 2); // true
+    console.log(2 == '3'); // true
+
+    // not equal operator
+    console.log(3 != 2); // true
+    console.log('hello' != 'Hell'); // true
+
+    // strict equal operator
+    console.log(2 === 2); // true
+    console.log(2 === '2'); // false
+
+    // strict not equal operator
+    console.log(2 !== '2'); // true
+    console.log(2 !== 2); // false
+*/
+
+// 3: Logical Operators in JavaScript
+
+/*  // logical AND
+    console.log(true && true); // true
+    console.log(true && false); // false
+
+    // logical OR
+    console.log(true || false); // true
+
+    // logical NOT
+    console.log(!true); // false
+
+    // 4: String operators in JavaScript
+    // concatenation operator
+    console.log('hello' + 'world');
+
+    let a = 'JavaScript';
+
+    a += ' tutorial';  // a = a + ' tutorial';
+    console.log(a);
+*/
+
+// 4: javascript operators 
+    // 1)JavaScript String
+    // String is used to store text. In JavaScript, strings are surrounded by quotes:
+
+/*    Single quotes: 'Hello'
+    Double quotes: "Hello"
+    Backticks: `Hello`
+    For example,
+
+    //strings example
+    const name = 'ram';
+    const name1 = "hari";
+    const result = `The names are ${name} and ${name1}`;
+    Single quotes and double quotes are practically the same and you can use either of them.
+
+    Backticks are generally used when you need to include variables or expressions into a string. This is done by wrapping variables or expressions with ${variable or expression} (template literal) as shown above.
+
+    You will learn about the use of backticks in the JavaScript String tutorial.
+*/
+
+    // 2)JavaScript Number
+    // Number represents integer and floating numbers (decimals and exponentials). For example,
+
+/*     const number1 = 3;
+    const number2 = 3.433;
+    const number3 = 3e5 // 3 * 10^5
+*/
+
+    // 3)JavaScript Boolean
+    // This data type represents logical entities. Boolean represents one of two values: true or false. It is easier to think of it as a yes/no switch. For example,
+    
+/*      x=2<3;
+        alert(x);
+*/
+    // 4)JavaScript undefined
+
+    // The undefined data type represents value that is not assigned. If a variable is declared but the value is not assigned, then the value of that variable will be undefined. For example,
+    
+/*   let name;
+    console.log(name); // undefined
+*/
+
+    // 5)JavaScript Object
+    // An object is a complex data type that allows us to store collections of data. For example,
+    
+/*    const student = {
+        firstName: 'ram',
+        lastName: null,
+        class: 10
+    };
+*/
+
+// JavaScript typeof
+// To find the type of a variable, you can use the typeof operator. For example,
+
+/*    const name = 'ram';
+    console.log(typeof(name)); // returns "string"
+
+    const number = 4;
+    console.log(typeof(number)); //returns "number"
+
+    const valueChecked = true;
+    console.log(typeof(valueChecked)); //returns "boolean"
+
+    const a = null;
+    console.log(typeof(a)); // returns "object"
+*/
